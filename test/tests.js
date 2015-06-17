96eb6fdfc2cea12e8ab2dbc718df32f35b1cfbff
Matthew Tole <matthewtole@gmail.com>
2015-06-17 22:50:03 +0100

    Add new toSunny function to convert color to its sunny value

--- test/tests.js
+++ test/tests.js
@@ -9,7 +9,7 @@ describe('GColor', function () {
       done();
     });
   });
-  
+
   describe('#fromHex', function () {
     it('should convert a 6 character hex into a GColor value', function (done) {
       expect(GColor.fromHex('000000')).to.equal(192);
@@ -18,7 +18,7 @@ describe('GColor', function () {
       done();
     });
   });
-  
+
   describe('#toHex', function () {
     it('should convert a GColor value into a 6 character hex string', function (done) {
       expect(GColor.toHex(192)).to.equal('000000');
@@ -26,8 +26,8 @@ describe('GColor', function () {
       expect(GColor.toHex(219)).to.equal('55AAFF');
       done();
     });
-  });  
-  
+  });
+
   describe('#expandHex', function () {
     it('should convert a GColor hex value into a 6 character hex string', function (done) {
       expect(GColor.expandHex('C0')).to.equal('000000');
@@ -35,8 +35,8 @@ describe('GColor', function () {
       expect(GColor.expandHex('DB')).to.equal('55AAFF');
       done();
     });
-  });  
-  
+  });
+
   describe('#shortHex', function () {
     it('should convert a 6 character hex into a GColor 2 character hex string', function (done) {
       expect(GColor.shortHex('000000')).to.equal('C0');
@@ -44,5 +44,14 @@ describe('GColor', function () {
       expect(GColor.shortHex('55AA55')).to.equal('D9');
       done();
     });
-  });  
+  });
+
+  describe('#toSunny', function () {
+    it('should convert a color into its sunny equivalent', function (done) {
+      expect(GColor.toSunny(GColor.fromHex('ff0055'))).to.equal(GColor.fromHex('e25874'));
+      expect(GColor.toSunny(GColor.fromHex('ff55aa'))).to.equal(GColor.fromHex('e37fa7'));
+      expect(GColor.toSunny(GColor.fromHex('005500'))).to.equal(GColor.fromHex('2b4a2c'));
+      done();
+    });
+  });
 });
